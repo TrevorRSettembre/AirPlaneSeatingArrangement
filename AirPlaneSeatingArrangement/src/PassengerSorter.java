19e1fefe9378d620cc98c03e1cc0cbb795fdadc4
elueddeke <evalueddeke26@gmail.com>
2024-03-18 16:35:17 -0400

    Finished Passenger Sorter

--- AirPlaneSeatingArrangement/src/PassengerSorter.java
+++ AirPlaneSeatingArrangement/src/PassengerSorter.java
@@ -5,30 +5,44 @@ public class PassengerSorter
 {
     private ArrayList<Passenger> femalePassengers = new ArrayList<Passenger>();
     private ArrayList<Passenger> malePassengers = new ArrayList<Passenger>();
+    private ArrayList<Passenger> age = new ArrayList<Passenger>();
     private ArrayList<Passenger> smallCarryOns = new ArrayList<Passenger>();
     private ArrayList<Passenger> mediumCarryOns = new ArrayList<Passenger>();
     private ArrayList<Passenger> largeCarryOns = new ArrayList<Passenger>();
+    private ArrayList<Passenger> firstClassSeats = new ArrayList<Passenger>();
+    private ArrayList<Passenger> economySeats = new ArrayList<Passenger>();
+    private ArrayList<Passenger> economyPlusSeats = new ArrayList<Passenger>();
+
 
     public PassengerSorter(ArrayList<Passenger>passengerList)
     {
-        femalePassengerSorter(passengerList);
-        malePassengerSorter(passengerList);
-        smallCarryOnSorter(passengerList);
-        mediumCarryOnSorter(passengerList);
-        largeCarryOnSorter(passengerList);
+        genderSorter(passengerList);
+        ageSorter(passengerList);
+        carryOnSorter(passengerList);
+        seatsSorter(passengerList);
     }
 
-    public void femalePassengerSorter(ArrayList<Passenger>passengerList)
+    public void genderSorter(ArrayList<Passenger>passengerList) 
     {
         for (int i = 0; i < passengerList.size(); i++)
         {
             Passenger currentPassenger = passengerList.get(i);
-            if (currentPassenger.getGender().equalsIgnoreCase("f"))
-                femalePassengers.add(currentPassenger);
+            String gender = currentPassenger.getGender().toLowerCase();
+            switch (gender) {
+                case "f":
+                    femalePassengers.add(currentPassenger);
+                    break;
+                case "m":
+                    malePassengers.add(currentPassenger);
+                    break;
+                default:
+                    System.out.println("Invalid gender");
+            }
         }
+
         for (int i = 0; i < femalePassengers.size(); i++)
         {
-            for (int j = i + 1; j < femalePassengers.size(); j++)
+            for (int j = i + 1; j < femalePassengers.size() - 1; j++)
             {
                 if (femalePassengers.get(j).getAge() < femalePassengers.get(i).getAge())
                 {
@@ -38,19 +52,10 @@ public class PassengerSorter
                 }
             }
         }
-    }
 
-    public void malePassengerSorter(ArrayList<Passenger>passengerList)
-    {
-        for (int i = 0; i < passengerList.size(); i++)
-        {
-            Passenger currentPassenger = passengerList.get(i);
-            if (currentPassenger.getGender().equalsIgnoreCase("m"))
-                malePassengers.add(currentPassenger);
-        }
         for (int i = 0; i < malePassengers.size(); i++)
         {
-            for (int j = i + 1; j < malePassengers.size(); j++)
+            for (int j = i + 1; j < malePassengers.size() - 1; j++)
             {
                 if (malePassengers.get(j).getAge() < malePassengers.get(i).getAge())
                 {
@@ -62,33 +67,64 @@ public class PassengerSorter
         }
     }
 
-    public void smallCarryOnSorter(ArrayList<Passenger>passengerList)
+    public void ageSorter(ArrayList<Passenger>passengerList)
     {
-        for (int i = 0; i < passengerList.size(); i++)
+        age = passengerList; 
+        for (int i = 0; i < age.size(); i++)
         {
-            Passenger currentPassenger = passengerList.get(i);
-            if (currentPassenger.getCarryonSize().equalsIgnoreCase("small"))
-                smallCarryOns.add(currentPassenger);
+            for (int j = i + 1; j < age.size() - 1; j++)
+            {
+                if (age.get(j).getAge() < age.get(i).getAge())
+                {
+                    Passenger temp = age.get(i);
+                    age.set(i, age.get(j));
+                    age.set(j, temp);
+                }
+            }
         }
     }
 
-    public void mediumCarryOnSorter(ArrayList<Passenger>passengerList)
+    public void carryOnSorter(ArrayList<Passenger>passengerList)
     {
         for (int i = 0; i < passengerList.size(); i++)
         {
             Passenger currentPassenger = passengerList.get(i);
-            if (currentPassenger.getCarryonSize().equalsIgnoreCase("medium"))
-                mediumCarryOns.add(currentPassenger);
+            String carryOn = currentPassenger.getCarryonSize().toLowerCase();
+            switch (carryOn) {
+                case "small":
+                    smallCarryOns.add(currentPassenger);
+                    break;
+                case "medium":
+                    mediumCarryOns.add(currentPassenger);
+                    break;
+                case "large":
+                    largeCarryOns.add(currentPassenger);;
+                    break;
+                default:
+                    System.out.println("Invalid carry-on size");
+            }
         }
     }
 
-    public void largeCarryOnSorter(ArrayList<Passenger>passengerList)
+    public void seatsSorter(ArrayList<Passenger>passengerList)
     {
         for (int i = 0; i < passengerList.size(); i++)
         {
             Passenger currentPassenger = passengerList.get(i);
-            if (currentPassenger.getCarryonSize().equalsIgnoreCase("large"))
-                largeCarryOns.add(currentPassenger);
+            String seatClass = currentPassenger.getSeatClass().toLowerCase();
+            switch (seatClass) {
+                case "f":
+                    firstClassSeats.add(currentPassenger);
+                    break;
+                case "e":
+                    economySeats.add(currentPassenger);
+                    break;
+                case "p":
+                    economyPlusSeats.add(currentPassenger);
+                    break;
+                default:
+                    System.out.println("Invalid seat class");
+            }
         }
     }
 
