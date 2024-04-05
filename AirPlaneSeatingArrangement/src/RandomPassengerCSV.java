2498644b86b1d41740c26c9da9e744afb8db5e28
Trevor Settembre <trevorrsettembre@gmail.com>
2024-04-04 21:43:25 -0400

    added seat numbers to econ and econ plus

--- AirPlaneSeatingArrangement/src/RandomPassengerCSV.java
+++ AirPlaneSeatingArrangement/src/RandomPassengerCSV.java
@@ -120,6 +120,8 @@ public class RandomPassengerCSV {
 
         ArrayList<Integer> passengerId = new ArrayList<Integer>();
         ArrayList<ArrayList<String>> firstClass = new ArrayList<ArrayList<String>>();
+        ArrayList<ArrayList<String>> econPlusClass = new ArrayList<ArrayList<String>>();
+        ArrayList<ArrayList<String>> econClass = new ArrayList<ArrayList<String>>();
         //List(first(groups(ids)) , second(groups), thrid(groups))
         ArrayList<ArrayList<ArrayList<String>>> passengerGroup = new ArrayList<ArrayList<ArrayList<String>>>();
      
@@ -128,11 +130,11 @@ public class RandomPassengerCSV {
         for(int i = 0; i < passengerCount;i++){
             passengerId.add(i+1);
         }
+
         //Also max of 4 to make it easy
         int row = 1;
         for (int i = 0; i < randomNumberGen.RandomInt(1, 4); i++) {
             ArrayList<String> innerList = new ArrayList<String>();
-
             // max group count for first class is 4 due to seat length to make it easier
             int maxPassengers = Math.min(randomNumberGen.RandomInt(1, 4), passengerId.size());
             for(int j = 0; j < maxPassengers; j++) {  
@@ -140,48 +142,209 @@ public class RandomPassengerCSV {
              //make better just need beta for sprint
                switch (row) {
                 case 1:
-                    innerList.add(String.valueOf(passengerId.get(randomNumberGen.RandomInt(0, (passengerId.size()-1)))).concat(selectedSeat.getRow1(0)));
-                    selectedSeat.removeIndexRow1(0);
+                    innerList.add(String.valueOf(passengerId.get(randomNumberGen.RandomInt(0, (passengerId.size()-1)))).concat(selectedSeat.getFirstClassRow(1,0)));
+                    selectedSeat.removeFirstClassRow(1, 0);
                     break;
                 case 2:
-                    innerList.add(String.valueOf(passengerId.get(randomNumberGen.RandomInt(0, (passengerId.size()-1)))).concat(selectedSeat.getRow2(0)));
-                    selectedSeat.removeIndexRow2(0);
+                    innerList.add(String.valueOf(passengerId.get(randomNumberGen.RandomInt(0, (passengerId.size()-1)))).concat(selectedSeat.getFirstClassRow(2,0)));
+                    selectedSeat.removeFirstClassRow(2, 0);
                     break;
                 case 3:
-                    innerList.add(String.valueOf(passengerId.get(randomNumberGen.RandomInt(0, (passengerId.size()-1)))).concat(selectedSeat.getRow3(0)));
-                    selectedSeat.removeIndexRow3(0);
+                    innerList.add(String.valueOf(passengerId.get(randomNumberGen.RandomInt(0, (passengerId.size()-1)))).concat(selectedSeat.getFirstClassRow(3,0)));
+                    selectedSeat.removeFirstClassRow(3, 0);
                     break;
                 case 4:
-                    innerList.add(String.valueOf(passengerId.get(randomNumberGen.RandomInt(0, (passengerId.size()-1)))).concat(selectedSeat.getRow4(0)));
-                    selectedSeat.removeIndexRow4(0);
+                    innerList.add(String.valueOf(passengerId.get(randomNumberGen.RandomInt(0, (passengerId.size()-1)))).concat(selectedSeat.getFirstClassRow(4,0)));
+                    selectedSeat.removeFirstClassRow(4, 0);
                     break;
                 
                
                 default:
                     break;
                }
-                // selectedSeat.removeIndexRow1(0);
-                //System.out.println(innerList.get(j));
                 String parsedId = innerList.get(j).substring(0,(innerList.get(j).length()-2));
-                // System.out.println(parsedId);
                 passengerId.remove(Integer.valueOf(parsedId));
-
             }
-            //Collections.sort(innerList);
             firstClass.add(innerList);
             row++;
         }
-        System.out.println(firstClass);
-        // System.out.println(passengerId);
+     
 
-        passengerGroup.add(firstClass);
-        // passengerGroup.add(econPlus);
-        // passengerGroup.add(econ);
+        passengerGroup.add(firstClass);       
 
+        //Econ plus---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        int row2 = 1;
+        for (int i = 0; i < randomNumberGen.RandomInt(1, 9); i++) {
+            ArrayList<String> innerList = new ArrayList<String>();
+            // max group count for first class is 6 due to seat length to make it easier
+            int maxPassengers = Math.min(randomNumberGen.RandomInt(1, 6), passengerId.size());
+            for(int j = 0; j < maxPassengers; j++) {  
+             //   innerList.add(String.valueOf(passengerId.get(randomNumberGen.RandomInt(0, (passengerId.size()-1)))).concat(selectedSeat.getRow1(0)));
+             //make better just need beta for sprint
+             //pretty much done but error when passenger id is single digith and seat is double
+             //hard coded error correction
+             int error = 10;
+               switch (row2) {
+                case 1:
+                    
+                    innerList.add(String.valueOf(passengerId.get(randomNumberGen.RandomInt(error, (passengerId.size()-1)))).concat(selectedSeat.getEconPlusClassRow(7,0)));
+                    selectedSeat.removeEconPLusClassRow(7, 0);
+                    break;
+                case 2:
+                    innerList.add(String.valueOf(passengerId.get(randomNumberGen.RandomInt(error, (passengerId.size()-1)))).concat(selectedSeat.getEconPlusClassRow(8,0)));
+                    selectedSeat.removeEconPLusClassRow(8, 0);
+                    break;
+                case 3:
+                    innerList.add(String.valueOf(passengerId.get(randomNumberGen.RandomInt(error, (passengerId.size()-1)))).concat(selectedSeat.getEconPlusClassRow(10,0)));
+                    selectedSeat.removeEconPLusClassRow(10, 0);
+                    break;
+                case 4:
+                    innerList.add(String.valueOf(passengerId.get(randomNumberGen.RandomInt(error, (passengerId.size()-1)))).concat(selectedSeat.getEconPlusClassRow(11,0)));
+                    selectedSeat.removeEconPLusClassRow(11, 0);
+                    break;
+                case 5:
+                    innerList.add(String.valueOf(passengerId.get(randomNumberGen.RandomInt(error, (passengerId.size()-1)))).concat(selectedSeat.getEconPlusClassRow(12,0)));
+                    selectedSeat.removeEconPLusClassRow(12, 0);
+                    break;
+                case 6:
+                    innerList.add(String.valueOf(passengerId.get(randomNumberGen.RandomInt(error, (passengerId.size()-1)))).concat(selectedSeat.getEconPlusClassRow(14,0)));
+                    selectedSeat.removeEconPLusClassRow(14, 0);
+                    break;
+                case 7:
+                    innerList.add(String.valueOf(passengerId.get(randomNumberGen.RandomInt(error, (passengerId.size()-1)))).concat(selectedSeat.getEconPlusClassRow(15,0)));
+                    selectedSeat.removeEconPLusClassRow(15, 0);
+                    break;
+                case 8:
+                    innerList.add(String.valueOf(passengerId.get(randomNumberGen.RandomInt(error, (passengerId.size()-1)))).concat(selectedSeat.getEconPlusClassRow(20,0)));
+                    selectedSeat.removeEconPLusClassRow(20, 0);
+                    break;
+                case 9:
+                    innerList.add(String.valueOf(passengerId.get(randomNumberGen.RandomInt(error, (passengerId.size()-1)))).concat(selectedSeat.getFirstClassRow(21,0)));
+                    selectedSeat.removeEconPLusClassRow(21, 0);
+                    break;
+  
+                default:
+                    break;
+               }
+               
 
-        //passengerGroup.get(firstclass).get(firstClass group one).get(id)
+              
+               String parsedId = "";
+               if(innerList.get(j).length() >= 5){
+                //156B12
+                //12B7
+                parsedId = innerList.get(j).substring(0,(innerList.get(j).length()-3));
+               }else{
+                parsedId = innerList.get(j).substring(0,(innerList.get(j).length()-2));
+               }
+              
+                passengerId.remove(Integer.valueOf(parsedId));
+            }
+            econPlusClass.add(innerList);
+            row2++;
+        }
+     
 
+        passengerGroup.add(econPlusClass);   
         
+        //Econ plus---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        int row3 = 1;
+        for (int i = 0; i < randomNumberGen.RandomInt(1, 16); i++) {
+            ArrayList<String> innerList = new ArrayList<String>();
+            // max group count for first class is 6 due to seat length to make it easier
+            int maxPassengers = Math.min(randomNumberGen.RandomInt(1, 6), passengerId.size());
+            for(int j = 0; j < maxPassengers; j++) {  
+             //   innerList.add(String.valueOf(passengerId.get(randomNumberGen.RandomInt(0, (passengerId.size()-1)))).concat(selectedSeat.getRow1(0)));
+             //make better just need beta for sprint
+             //pretty much done but error when passenger id is single digith and seat is double
+             //hard coded error correction
+             int error = 10;
+               switch (row3) {
+                case 1:
+                    innerList.add(String.valueOf(passengerId.get(randomNumberGen.RandomInt(error, (passengerId.size()-1)))).concat(selectedSeat.getEconClassRow(22,0)));
+                    selectedSeat.removeEconClassRow(22, 0);
+                    break;
+                case 2:
+                    innerList.add(String.valueOf(passengerId.get(randomNumberGen.RandomInt(error, (passengerId.size()-1)))).concat(selectedSeat.getEconClassRow(23,0)));
+                    selectedSeat.removeEconClassRow(23, 0);
+                    break;
+                case 3:
+                    innerList.add(String.valueOf(passengerId.get(randomNumberGen.RandomInt(error, (passengerId.size()-1)))).concat(selectedSeat.getEconClassRow(24,0)));
+                    selectedSeat.removeEconClassRow(24, 0);
+                    break;
+                case 4:
+                    innerList.add(String.valueOf(passengerId.get(randomNumberGen.RandomInt(error, (passengerId.size()-1)))).concat(selectedSeat.getEconClassRow(25,0)));
+                    selectedSeat.removeEconClassRow(25, 0);
+                    break;
+                case 5:
+                    innerList.add(String.valueOf(passengerId.get(randomNumberGen.RandomInt(error, (passengerId.size()-1)))).concat(selectedSeat.getEconClassRow(26,0)));
+                    selectedSeat.removeEconClassRow(26, 0);
+                    break;
+                case 6:
+                    innerList.add(String.valueOf(passengerId.get(randomNumberGen.RandomInt(error, (passengerId.size()-1)))).concat(selectedSeat.getEconClassRow(27,0)));
+                    selectedSeat.removeEconClassRow(27, 0);
+                    break;
+                case 7:
+                    innerList.add(String.valueOf(passengerId.get(randomNumberGen.RandomInt(error, (passengerId.size()-1)))).concat(selectedSeat.getEconClassRow(28,0)));
+                    selectedSeat.removeEconClassRow(28, 0);
+                    break;
+                case 8:
+                    innerList.add(String.valueOf(passengerId.get(randomNumberGen.RandomInt(error, (passengerId.size()-1)))).concat(selectedSeat.getEconClassRow(29,0)));
+                    selectedSeat.removeEconClassRow(29, 0);
+                    break;
+                case 9:
+                    innerList.add(String.valueOf(passengerId.get(randomNumberGen.RandomInt(error, (passengerId.size()-1)))).concat(selectedSeat.getEconClassRow(30,0)));
+                    selectedSeat.removeEconClassRow(30, 0);
+                    break;
+                case 10:
+                    innerList.add(String.valueOf(passengerId.get(randomNumberGen.RandomInt(error, (passengerId.size()-1)))).concat(selectedSeat.getEconClassRow(31,0)));
+                    selectedSeat.removeEconClassRow(31, 0);
+                    break;
+                case 11:
+                    innerList.add(String.valueOf(passengerId.get(randomNumberGen.RandomInt(error, (passengerId.size()-1)))).concat(selectedSeat.getEconClassRow(32,0)));
+                    selectedSeat.removeEconClassRow(32, 0);
+                    break;
+                case 12:
+                    innerList.add(String.valueOf(passengerId.get(randomNumberGen.RandomInt(error, (passengerId.size()-1)))).concat(selectedSeat.getEconClassRow(34,0)));
+                    selectedSeat.removeEconClassRow(34, 0);
+                    break;
+                case 13:
+                    innerList.add(String.valueOf(passengerId.get(randomNumberGen.RandomInt(error, (passengerId.size()-1)))).concat(selectedSeat.getEconClassRow(35,0)));
+                    selectedSeat.removeEconClassRow(35, 0);
+                    break;
+                case 14:
+                    innerList.add(String.valueOf(passengerId.get(randomNumberGen.RandomInt(error, (passengerId.size()-1)))).concat(selectedSeat.getEconClassRow(36,0)));
+                    selectedSeat.removeEconClassRow(36, 0);
+                    break;
+                case 15:
+                    innerList.add(String.valueOf(passengerId.get(randomNumberGen.RandomInt(error, (passengerId.size()-1)))).concat(selectedSeat.getEconClassRow(37,0)));
+                    selectedSeat.removeEconClassRow(37, 0);
+                    break;
+                case 16:
+                    innerList.add(String.valueOf(passengerId.get(randomNumberGen.RandomInt(error, (passengerId.size()-1)))).concat(selectedSeat.getEconClassRow(38,0)));
+                    selectedSeat.removeEconClassRow(38, 0);
+                    break;
+                default:
+                    break;
+               }
+               
+
+           
+               String parsedId = "";
+               if(innerList.get(j).length() >= 5){
+                parsedId = innerList.get(j).substring(0,(innerList.get(j).length()-3));
+               }else{
+                parsedId = innerList.get(j).substring(0,(innerList.get(j).length()-2));
+               }
+              
+                passengerId.remove(Integer.valueOf(parsedId));
+            }
+            econClass.add(innerList);
+            row3++;
+        }
+     
+
+        passengerGroup.add(econClass);   
        
        return passengerGroup;
 
@@ -213,8 +376,6 @@ public class RandomPassengerCSV {
             passengerStats.add(namesGender[2]);
             passengerStats.add(generateAge());
             passengerStats.add(carryon[0]);
-            //remove weight
-            passengerStats.add(carryon[1]);
             /**
              * 0 = first
              * 1 = econplus
@@ -230,6 +391,57 @@ public class RandomPassengerCSV {
                     if(id == Integer.parseInt(groups.get(0).get(i).get(j).substring(0, groups.get(0).get(i).get(j).length() - 2))){
                         passengerStats.add(groups.get(0).get(i).get(j).substring(Math.max(groups.get(0).get(i).get(j).length() - 2, 0)));
                     }
+                }
+
+
+            }
+            /**
+             * 0 = first
+             * 1 = econplus
+             * 2 = econ
+             * 
+             */
+            //passengerGroup.get(firstclass).get(firstClass group one).get(id)
+            //Length of econ plus class 7,8,10,11,12,14,15,20,21
+            for(int i = 0; i < groups.get(1).size(); i++){
+                //first class group index 
+                for(int j = 0; j < groups.get(1).get(i).size();j++){
+                    if(groups.get(1).get(i).get(j).length() >= 5){
+                        if(id == Integer.parseInt(groups.get(1).get(i).get(j).substring(0, groups.get(1).get(i).get(j).length() - 3))){
+                            passengerStats.add(groups.get(1).get(i).get(j).substring(Math.max(groups.get(1).get(i).get(j).length() - 3, 0)));
+                        }
+                    }else{
+                        if(id == Integer.parseInt(groups.get(1).get(i).get(j).substring(0, groups.get(1).get(i).get(j).length() - 2))){
+                            passengerStats.add(groups.get(1).get(i).get(j).substring(Math.max(groups.get(1).get(i).get(j).length() - 2, 0)));
+                        }
+                    }
+                    
+                }
+
+
+            }
+
+             /**
+             * 0 = first
+             * 1 = econplus
+             * 2 = econ
+             * 
+             */
+            //passengerGroup.get(firstclass).get(firstClass group one).get(id)
+            //Length of econ plus class 7,8,10,11,12,14,15,20,21
+            for(int i = 0; i < groups.get(2).size(); i++){
+                //first class group index 
+                for(int j = 0; j < groups.get(2).get(i).size();j++){
+                    if(groups.get(2).get(i).get(j).length() >= 5){
+                        if(id == Integer.parseInt(groups.get(2).get(i).get(j).substring(0, groups.get(2).get(i).get(j).length() - 3))){
+                            passengerStats.add(groups.get(2).get(i).get(j).substring(Math.max(groups.get(2).get(i).get(j).length() - 3, 0)));
+                        }
+                    }else{
+                        if(id == Integer.parseInt(groups.get(2).get(i).get(j).substring(0, groups.get(2).get(i).get(j).length() - 2))){
+                            passengerStats.add(groups.get(2).get(i).get(j).substring(Math.max(groups.get(2).get(i).get(j).length() - 2, 0)));
+                        }
+                    }
+                    
                 }
 
 
