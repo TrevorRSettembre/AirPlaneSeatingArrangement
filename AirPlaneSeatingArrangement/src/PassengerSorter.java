f11003705b424aa25458e6795e35f3770ff372bf
elueddeke <evalueddeke26@gmail.com>
2024-04-18 14:30:22 -0400

    Update on my end

--- AirPlaneSeatingArrangement/src/PassengerSorter.java
+++ AirPlaneSeatingArrangement/src/PassengerSorter.java
@@ -20,8 +20,9 @@ public class PassengerSorter
         gender = new ArrayList<Passenger>();
         for (int i = 0; i < passengerList.size(); i++) {
             Passenger currentPassenger = passengerList.get(i);
-            if (currentPassenger.getGender().toLowerCase().equals("f"))
+            if (currentPassenger.getGender().toLowerCase().equals("female")) {
                 gender.add(currentPassenger);
+            }
         }
     }
 
@@ -31,7 +32,7 @@ public class PassengerSorter
         gender = new ArrayList<Passenger>();
         for (int i = 0; i < passengerList.size(); i++) {
             Passenger currentPassenger = passengerList.get(i);
-            if (currentPassenger.getGender().toLowerCase().equals("m"))
+            if (currentPassenger.getGender().toLowerCase().equals("male"))
                 gender.add(currentPassenger);
         }
     }
